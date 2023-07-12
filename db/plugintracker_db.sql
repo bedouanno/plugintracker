1ab5b9feb540e3282a4387661b8ca5f0f5ddfef1
Obed Ouano <obed.ouano@proweaver.net>
2023-07-12 10:28:57 +0800

    1743388 - UP

--- db/plugintracker_db.sql
+++ db/plugintracker_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 12, 2023 at 04:21 AM
+-- Generation Time: Jul 12, 2023 at 04:27 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.10
 
